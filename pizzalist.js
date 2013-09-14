ecb6475281844e93d721039e54f6da779cd26fe0
Johan Lindskogen <johan.lindskogen@gmail.com>
2013-09-14 02:30:39 +0200

    Adding buttons for incrementing/decremening

--- pizzalist.js
+++ pizzalist.js
@@ -10,27 +10,24 @@ if (Meteor.isClient) {
   });
 
   Template.pizzalist.entries = function () {
-    return Orders.find({}, {sort: {score: -1, name: 1}});
-  };
-
-  Template.pizzalist.selected_name = function () {
-    var entry = Orders.findOne(Session.get("selected_player"));
-    return entry && entry.name;
-  };
-
-  Template.pizzalist.selected = function () {
-    return Session.equals("selected_player", this._id) ? "selected" : '';
+    return Orders.find({}, {sort: {name: 1}});
   };
 
   Template.pizzalist.events({
-    'click input.inc': function () {
-      Orders.update(Session.get("selected_player"), {$inc: {score: 5}});
-    }
-  });
-
-  Template.entry.events({
-    'click': function () {
-      Session.set("selected_player", this._id);
+    'keydown, click input.inc': function (e) {
+      if (e.type === 'click' || e.type === 'keydown' && e.which === 13) {
+        var el = document.getElementById('pizzaname');
+        if (el.value.length > 0)  {
+          Orders.insert({name: el.value, amount: 1});
+          el.value = "";
+        }
+      }
+    },
+    'click .action.plus': function () {
+      Orders.update(this._id, {$inc: {amount: 1}});
+    },
+    'click .action.minus': function () {
+      Orders.update(this._id, {$inc: {amount: -1}});
     }
   });
 }
@@ -38,15 +35,17 @@ if (Meteor.isClient) {
 // On server startup, create some players if the database is empty.
 if (Meteor.isServer) {
   Meteor.startup(function () {
-    if (Orders.find().count() === 0) {
-      var names = ["Ada Lovelace",
-                   "Grace Hopper",
-                   "Marie Curie",
-                   "Carl Friedrich Gauss",
-                   "Nikola Tesla",
-                   "Claude Shannon"];
-      for (var i = 0; i < names.length; i++)
-        Orders.insert({name: names[i], score: Math.floor(Random.fraction()*10)*5});
-    }
+    // Nothing yet!
+
+    // if (Orders.find().count() === 0) {
+    //   var names = ["Ada Lovelace",
+    //                "Grace Hopper",
+    //                "Marie Curie",
+    //                "Carl Friedrich Gauss",
+    //                "Nikola Tesla",
+    //                "Claude Shannon"];
+    //   for (var i = 0; i < names.length; i++)
+    //     Orders.insert({name: names[i], amount: Math.floor(Random.fraction()*10)*5});
+    // }
   });
 }
